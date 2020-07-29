dfd1a16e48e9d5e04df14d12c3d49d0697eef3cd
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2020-07-29 15:33:48 +0100

    Bump version: 0.0.7 → 0.0.8

--- domdf_sphinx_theme/__init__.py
+++ domdf_sphinx_theme/__init__.py
@@ -33,7 +33,7 @@ __author__: str = "Dominic Davis-Foster"
 __copyright__: str = "2020 Dominic Davis-Foster"
 
 __license__: str = "MIT License"
-__version__: str = "0.0.7"
+__version__: str = "0.0.8"
 __email__: str = "dominic@davis-foster.co.uk"
 
 __version_full__ = __version__
