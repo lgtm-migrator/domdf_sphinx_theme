a09f23468d157d2965fb37def0ff8e2da028392d
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2020-07-29 12:29:05 +0100

    Trying to fix ReadTheDocs button.

--- domdf_sphinx_theme/static/js/rtfd.js
+++ domdf_sphinx_theme/static/js/rtfd.js
@@ -1,6 +1,6 @@
-const destination = document.getElementById("rtfd_button_dest");
-const to_remove = document.getElementById("rtfd_button_to_remove");
-const source = document.querySelector(".injected div.rst-versions div.rst-other-versions");
+const destination = $('#rtfd_button_dest');
+const to_remove = $('#rtfd_button_to_remove');
+const source = $(".injected div.rst-versions div.rst-other-versions");
 
 destination.onload = function(){
     to_remove.replaceWith(source)
