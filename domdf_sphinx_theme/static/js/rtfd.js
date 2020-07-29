52e81cc651e3893a00024de9e5555a3e0857e5da
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2020-07-29 15:29:24 +0100

    Removed old js

--- domdf_sphinx_theme/static/js/rtfd.js
+++
@@ -1,5 +0,0 @@
-const destination = document.getElementById("rtfd_button_dest");
-
-destination.onload = function(){
-    $('#rtfd_button_to_remove').replaceWith($(".injected div.rst-versions div.rst-other-versions"))
-};
