17dda7e8c1ad4db072e8c2d4e57b26df06f45732
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2021-03-04 15:32:03 +0000

    Use "whey" as the build backend.

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@
 # This file is managed by 'repo_helper'. Don't edit it directly.
 
 # stdlib
+import shutil
 import sys
 
 # 3rd party
@@ -12,13 +13,12 @@ sys.path.append('.')
 # this package
 from __pkginfo__ import *  # pylint: disable=wildcard-import
 
-
-
 setup(
-		description='Customised "sphinx_rtd_theme" used by my Python projects.',
+		description="Customised 'sphinx_rtd_theme' used by my Python projects.",
 		extras_require=extras_require,
 		install_requires=install_requires,
 		py_modules=[],
 		version=__version__,
-		entry_points={"sphinx.html_themes": ["domdf_sphinx_theme = domdf_sphinx_theme"]},
 		)
+
+shutil.rmtree("domdf_sphinx_theme.egg-info", ignore_errors=True)
